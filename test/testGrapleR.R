fe226aadda2d53e886a482379953c47efaace3e5
Satish <smahesul@ufl.edu>
2016-02-11 14:07:24 -0500

    Removed unnecessary code

--- test/testGrapleR.R
+++ test/testGrapleR.R
@@ -37,7 +37,6 @@ setwd(simDir)
 expId3 <- GrapleRunExperimentSweep(graplerURL, simDir, driverFileName, parameterName, startValue, endValue, numberOfIncrements)
 GrapleCheckExperimentCompletion(graplerURL, expId3)
 GrapleGetExperimentResults(graplerURL, expId3)
-#GrapleGetSimResult(graplerURL, expId, 3)
 
 #Experiment 4
 simDir="c:/Workspace/SimRoot1/Exp4"
@@ -51,16 +50,13 @@ setwd(simDir)
 expId4<-GrapleRunExperimentSweep(graplerURL, simDir, driverFileName, parameterName, startValue, endValue, numberOfIncrements, filterName)
 GrapleCheckExperimentCompletion(graplerURL, expId4)
 GrapleGetExperimentResults(graplerURL, expId4)
-#GrapleGetSimResult(graplerURL, expId, 3)
 
 #Experiment 5
-#simDir="c:/Workspace/SimRoot/Exp5"
 simDir="c:/Workspace/SimRoot1/Exp5"
 setwd(simDir)
 expId5 <- GrapleRunExperimentJob(graplerURL, simDir)
 GrapleCheckExperimentCompletion(graplerURL, expId5)
 GrapleGetExperimentJobResults(graplerURL, expId5)
-#GrapleGetSimResult(graplerURL, expId, 5)
 
 #Experiment 6
 simDir="C:/Workspace/SimRoot/Exp6"
@@ -69,4 +65,3 @@ setwd(simDir)
 expId6<-GrapleRunExperimentJob(graplerURL, simDir, filterName)
 GrapleCheckExperimentCompletion(graplerURL, expId6)
 GrapleGetExperimentJobResults(graplerURL, expId6)
-#GrapleGetSimResult(graplerURL, expId, 5)
