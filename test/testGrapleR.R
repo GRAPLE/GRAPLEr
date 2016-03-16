15ab9bff2da6b9be707be84d497fbd2ed6221ab1
Jaikrishna <t.s.jaikrishna@gmail.com>
2016-03-16 13:35:56 -0400

    Revert testGrapleR.R changes

--- test/testGrapleR.R
+++ test/testGrapleR.R
@@ -1,33 +1,32 @@
-#install_github("GRAPLE/GrapleR")
+#install.packages("devtools")
+#devtools::install_github("klutometis/roxygen")
+#devtools::install_github("GRAPLE/GRAPLEr")
 library("bitops")
 library("RCurl")
-library("jsonlite")
-library('GLMr')
-library("glmtools")
 library("jsonlite")
 library("GRAPLEr")
 
 graplerURL<-"http://graple.acis.ufl.edu"
 print(GrapleCheckService(graplerURL))
 
-#Experiment 1
-expRootDir<-"~/work/SimRoot1/Exp1"
+#Experiment 1: Your own handcrafted simulations
+expRootDir<-"c:/ExpRoot/Exp1"
 setwd(expRootDir)
 expId1<-GrapleRunExperiment(graplerURL, expRootDir)
 GrapleCheckExperimentCompletion(graplerURL, expId1)
 GrapleGetExperimentResults(graplerURL, expId1)
-GrapleAbortExperiment(graplerURL, expId1)
 
-#Experiment 2 - w/ Filter
-expRootDir<-"~/work/SimRoot1/Exp2"
+#Experiment 2 - Your own handcrafted simulations w/ post processing R-Filter(experimental feature)
+expRootDir<-"c:/ExpRoot/Exp2"
 filterName <- "Filter1.R"
 setwd(expRootDir)
 expId2<-GrapleRunExperiment(graplerURL, expRootDir, filterName)
 GrapleCheckExperimentCompletion(graplerURL, expId2)
 GrapleGetExperimentResults(graplerURL, expId2)
 
-#Experiment 3
-simDir3="~/work/SimRoot1/Exp3"
+#Experiment 3: An increment type sweep experiment. 
+#Paramters are passed directly to funtion on invocation.
+simDir3="c:/ExpRoot/Exp3"
 driverFileName="met_hourly.csv"
 parameterName="AirTemp"
 startValue=-2
@@ -38,8 +37,8 @@ expId3 <- GrapleRunExperimentSweep(graplerURL, simDir3, driverFileName, paramete
 GrapleCheckExperimentCompletion(graplerURL, expId3)
 GrapleGetExperimentResults(graplerURL, expId3)
 
-#Experiment 4 - w/ Filter
-simDir4="~/work/SimRoot1/Exp4"
+#Experiment 4: An increment type sweep experiment w/ post processing R-Filter(experimental feature)
+simDir4="c:/ExpRoot/Exp4"
 driverFileName="met_hourly.csv"
 parameterName="AirTemp"
 startValue=-2
@@ -51,23 +50,25 @@ expId4<-GrapleRunExperimentSweep(graplerURL, simDir4, driverFileName, parameterN
 GrapleCheckExperimentCompletion(graplerURL, expId4)
 GrapleGetExperimentResults(graplerURL, expId4)
 
-#Experiment 5
-simDir5="~/work/SimRoot1/Exp5"
+#Experiment 5: A sweep experiment using using various distributions for generating ranges. 
+#Paramters specified via an input file.
+simDir5="c:/ExpRoot/Exp5"
 setwd(simDir5)
 expId5 <- GrapleRunExperimentJob(graplerURL, simDir5)
 GrapleCheckExperimentCompletion(graplerURL, expId5)
 GrapleGetExperimentJobResults(graplerURL, expId5)
 
-#Experiment 6 - w/ Filter
-simDir6="~/work/SimRoot1/Exp6"
+#Experiment 6: A sweep experiment using using various distributions for generating ranges
+#w/ post processing R-Filter(experimental feature)
+simDir6="C:/ExpRoot/Exp6"
 filterName = "Filter1.R"
 setwd(simDir6)
 expId6<-GrapleRunExperimentJob(graplerURL, simDir6, filterName)
 GrapleCheckExperimentCompletion(graplerURL, expId6)
 GrapleGetExperimentJobResults(graplerURL, expId6)
 
-#Experiment 7
-expRootDir<-"~/work/SimRoot1/Exp1"
+#Experiment 7: Aborting an active or failed experiment
+expRootDir<-"c:/ExpRoot/Exp1"
 setwd(expRootDir)
 expId1<-GrapleRunExperiment(graplerURL, expRootDir)
 GrapleCheckExperimentCompletion(graplerURL, expId1)
