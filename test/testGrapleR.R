9582baba73e9e5b7de07c206edd5b7d73afad5f9
Satish <smahesul@ufl.edu>
2016-04-15 15:24:00 -0400

    modified set Exp Name and added additional methods available

--- test/testGrapleR.R
+++ test/testGrapleR.R
@@ -3,7 +3,9 @@ library(RCurl)
 library(jsonlite)
 library(GRAPLEr)
 
+# Default SubmissionURL is set to http://graple.acis.ufl.edu, use setSubmissionURL method to change submissionURL
 graple <- Graple()
+
 #The result of the method call is stored in the object itself
 graple <- GrapleCheckService(graple)
 print(graple@StatusMsg)
@@ -16,8 +18,15 @@ print(graple@StatusMsg)
 #
 #Start a new experiment and setup parameters
 grapleExp1 <- Graple(ExpRootDir="D:/GRAPLE/ExpRoot/Exp1", ResultsDir="D:/GRAPLE/Results/Exp1", TempDir = tempdir())
-grapleExp1@ExpName="BatchExperiment1"
-grapleExp1 <- setSubmissionURL(grapleExp1, "http://graple.acis.ufl.edu")
+grapleExp1 <- setExpName(grapleExp1, "BatchExperiment1")
+
+# Change SubmissionURL
+#grapleExp1 <- setSubmissionURL(grapleExp1, "http://10.244.37.4:5000")
+
+# Change directories
+#grapleExp1 <- setExperimentDir(grapleExp1, "C:/ExpRoot/Exp1")
+#grapleExp1 <- setResultsDir(grapleExp1, "C:/ExpRoot/Results/Exp1")
+#grapleExp1 <- setTempDir(grapleExp1, "C:/TempDir")
 
 #Run the experiment
 grapleExp1 <- GrapleRunExperiment(grapleExp1);
@@ -49,8 +58,8 @@ grapleExp2 <- GrapleGetExperimentResults(grapleExp2);
 #
 grapleExp3 <- Graple(ExpRootDir="D:/GRAPLE/ExpRoot/Exp3", ResultsDir="D:/GRAPLE/Results/Exp3", TempDir = tempdir())
 grapleExp4 <- Graple(ExpRootDir="D:/GRAPLE/ExpRoot/Exp4", ResultsDir="D:/GRAPLE/Results/Exp4", TempDir = tempdir())
-grapleExp3@ExpName="SweepExperiment3"
-grapleExp4@ExpName="SweepExperiment4"
+grapleExp3 <- setExpName(grapleExp3, "SweepExperiment3")
+grapleExp4 <- setExpName(grapleExp4, "SweepExperiment4")
 grapleExp3 <- GrapleRunSweepExperiment(grapleExp3)
 print(grapleExp3@StatusMsg)
 grapleExp4 <- GrapleRunSweepExperiment(grapleExp4)
@@ -64,7 +73,7 @@ while (grapleExp3@StatusMsg != '100.0% complete' && grapleExp4@StatusMsg != '100
   print(paste(grapleExp3@ExpName, grapleExp3@StatusMsg, sep=":"))
 
   grapleExp4 <- GrapleCheckExperimentCompletion(grapleExp4)
-  print(paste(grapleExp3@ExpName, grapleExp4@StatusMsg, sep = ":"))
+  print(paste(grapleExp4@ExpName, grapleExp4@StatusMsg, sep = ":"))
 }
 
 grapleExp3 <- GrapleGetExperimentJobResults(grapleExp3)
